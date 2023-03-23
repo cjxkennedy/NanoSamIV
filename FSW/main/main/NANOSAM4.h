a9f0424fe4f91e6aa4d2227db6700c2222cde172
CJ Kennedy <55965129+cjxkennedy@users.noreply.github.com>
2023-03-22 21:27:36 -0600

    removed variable

--- FSW/main/main/NANOSAM4.h
+++ FSW/main/main/NANOSAM4.h
@@ -13,11 +13,14 @@ extern bool mode2;                   // "manual mode"
 extern bool mode3;                   // "threshold irradiance mode"
 extern bool mode4;                   // somewhat tested
 extern short mode4time;              // time input in seconds
-extern short mode4count;             // count of samples
 
 // Irradiance Threshold Parameters
 extern uint16_t threshold;           // dummy value
 extern uint16_t thresholdCount;      // # samples below threshold
+
+// Sun Condition
+extern bool sunsetCondition;
+extern bool sunriseCondition;
 
 // PINS
 extern const int PIN_ADC_CS;         // ADC Chip Select Pin
@@ -29,11 +32,13 @@ extern const int PIN_OPTICS_THERM;   // optics bench thermistor pin
 extern const int ADC_MAX_SPEED;      // 2 MHz
 
 // Function Prototypes
-void setup();
 void commandHandling();
+void dataCollection();
 void mode3Check();
 void mode4Check();
 int getMessageFromSerial();
+uint16_t scienceData();
 byte thresholdCheck(uint16_t data);
+byte sunsetSunrise();
 
 #endif
